c63de4efd476a1669ba2cf77a21a653788fd2c51
akasrai <akasky70@gmail.com>
2022-08-30 11:59:41 +0545

    Add glass effect in header navbar

---
+++ js/script.js
@@ -0,0 +1,13 @@
+function addGlassEffectHeader() {
+  if (this.window.scrollY > 100) {
+    select("header").addClass("box-shadow");
+    return select("header").addClass("glass-effect");
+  }
+
+  select("header").addClass("box-shadow");
+  return select("header").removeClass("glass-effect");
+}
+
+window.addEventListener("scroll", function () {
+  addGlassEffectHeader();
+});
